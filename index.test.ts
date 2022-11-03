25d566838d77883c66cc21c7ca9921b498ae737a
search <search35453@gmail.com>
2022-11-03 16:19:09 +0700

    implemented tests + merge function

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,83 @@
-import { hello } from "./index";
+import { merge } from "./index";
 
 test("test 1", () => {
-	expect(hello() === "hello world");
+	const collection_1: Array<number> = [1, 3, 5, 7];
+	const collection_2: Array<number> = [2, 4, 5, 8, 9];
+	const result = [...collection_1, ...collection_2].sort();
+	expect(merge(collection_1, collection_2)).toStrictEqual(result);
+});
+
+test("test 2", () => {
+	const collection_1: Array<number> = [];
+	const collection_2: Array<number> = [1, 2, 5, 8, 12];
+	const result = collection_2.sort();
+	expect(merge(collection_1, collection_2)).toStrictEqual(result);
+});
+
+test("test 3", () => {
+	const collection_1: Array<number> = [3, 4, 8];
+	const collection_2: Array<number> = [];
+	const result = collection_1.sort();
+	expect(merge(collection_1, collection_2)).toStrictEqual(result);
+});
+
+test("test 4", () => {
+	const collection_1 = undefined;
+	const collection_2: Array<number> = [];
+	const result = collection_2;
+	expect(merge(collection_1, collection_2)).toStrictEqual(result);
+});
+
+test("test 5", () => {
+	const collection_1: Array<number> = [];
+	const collection_2 = undefined;
+	const result = collection_1;
+	expect(merge(collection_1, collection_2)).toStrictEqual(result);
+});
+
+test("test 6", () => {
+	const collection_1 = undefined;
+	const collection_2 = undefined;
+	const result = undefined;
+	expect(merge(collection_1, collection_2)).toStrictEqual(result);
+});
+
+test("test 7", () => {
+	const collection_1 = null;
+	const collection_2: Array<number> = [];
+	const result = collection_2;
+	expect(merge(collection_1, collection_2)).toStrictEqual(result);
+});
+
+test("test 8", () => {
+	const collection_1: Array<number> = [];
+	const collection_2 = null;
+	const result = collection_1;
+	expect(merge(collection_1, collection_2)).toStrictEqual(result);
+});
+
+test("test 9", () => {
+	const collection_1 = null;
+	const collection_2 = null;
+	const result = undefined;
+	expect(merge(collection_1, collection_2)).toStrictEqual(result);
+});
+
+test("test 10", () => {
+	const getRandomInt = (min: number, max: number): number => {
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	};
+	const generateRandomSortedSequence = () => {
+		const seq = [];
+		const len = getRandomInt(1, 100);
+		for (let i = 0; i < len; i++) {
+			seq.push(getRandomInt(0, 100));
+		}
+		seq.sort((a, b) => a - b);
+		return seq;
+	};
+	const collection_1: Array<number> = generateRandomSortedSequence();
+	const collection_2: Array<number> = generateRandomSortedSequence();
+	const result = [...collection_1, ...collection_2].sort((a, b) => a - b);
+	expect(merge(collection_1, collection_2)).toStrictEqual(result);
 });
--- index.ts
+++ index.ts
@@ -1,3 +1,36 @@
-export function hello(): string {
-	return "hello world";
-}
+export const merge = (
+	collection_1: Array<number> | null | undefined,
+	collection_2: Array<number> | null | undefined
+): Array<number> | undefined => {
+	if (collection_1 === null || collection_1 === undefined) {
+		if (collection_2) return collection_2;
+		else return undefined;
+	}
+	if (collection_2 === null || collection_2 === undefined) {
+		if (collection_1) return collection_1;
+		else return undefined;
+	}
+	const length = collection_1.length + collection_2.length;
+	const merged = [];
+	let pointer1 = 0;
+	let pointer2 = 0;
+	for (let i = 0; i < length; i++) {
+		if (collection_1[pointer1] >= collection_2[pointer2]) {
+			if (collection_2[pointer2] || collection_2[pointer2] === 0)
+				merged.push(collection_2[pointer2]);
+			pointer2++;
+		} else if (collection_1[pointer1] < collection_2[pointer2]) {
+			if (collection_1[pointer1] || collection_1[pointer1] === 0)
+				merged.push(collection_1[pointer1]);
+			pointer1++;
+		}
+	}
+	for (let i = pointer1; i < collection_1.length; i++) {
+		merged.push(collection_1[i]);
+	}
+	for (let i = pointer2; i < collection_2.length; i++) {
+		merged.push(collection_2[i]);
+	}
+	console.log("MERGED", merged);
+	return merged;
+};
